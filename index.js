fd54c8f76388240893ad46c9e640097865d1530c
Yusuf Dumlu <yusuf.ex@gmail.com>
2023-02-04 10:18:33 +0300

    completed

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+const num1 =2
+const num2 =31
+const multiply =num1*num2
+const random = Math.floor(Math.random() * 10) + 1;
+
+
+const num3 =5
+const num4 =39
+const mod =num4%num3
+const max =Math.max(1,2,20)
+
+
+console.log(multiply,random,mod,max)
